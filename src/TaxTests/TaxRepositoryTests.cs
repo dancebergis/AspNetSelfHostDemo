346f156f976af2829b738c7f151a8a2906f56bfb
Marius Lozda <marius.lozda@adform.com>
2017-02-19 22:58:54 +0200

    tests update

--- src/TaxTests/TaxRepositoryTests.cs
+++ src/TaxTests/TaxRepositoryTests.cs
@@ -17,12 +17,16 @@ namespace TaxTests
         }
 
         [Test]
-        public void AddsYearlyTaxCorrectly()
+        public void AddsYearlyTaxCorrectly_ForDifferentCities()
         {
-             _sut.AddYearlyTax("a", 2016, 0.1m);
-            var result = _sut.GetTax("a", DateTime.Parse("2016.02.02"));
+            _sut.AddYearlyTax("a", 2016, 0.1m);
+            _sut.AddYearlyTax("b", 2016, 0.7m);
 
+            var result = _sut.GetTax("a", DateTime.Parse("2016.02.02"));
             Assert.That(result, Is.EqualTo(0.1m));
+
+            result = _sut.GetTax("b", DateTime.Parse("2016.12.02"));
+            Assert.That(result, Is.EqualTo(0.7m));
         }
 
         [Test]
@@ -49,6 +53,7 @@ namespace TaxTests
         {
             _sut.AddYearlyTax("a", 2016, 0.1m);
             _sut.AddMonthlyTax("a", 2016, 5, 0.2m);
+            _sut.AddMonthlyTax("b", 2016, 4, 0.5m);
             var result = _sut.GetTax("a", DateTime.Parse("2016.05.02"));
             Assert.That(result, Is.EqualTo(0.2m));
 
